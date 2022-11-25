dd3b58b2e36f1f3afb71fec5f9eb06d92947dee0
Jonathan Moreno <johnfrom209@gmail.com>
2022-11-25 01:43:08 -0800

    Thought model: thoughtText, createdAt, username, reactions, and virtual to get reactions count

---
+++ models/Thought.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 8,
+            maxLength: 200
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            // getter to format the timestamp on query
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: {
+                reactionCount: {
+                    get() {
+                        return reactions.length
+                    }
+                }
+            }
+        }
+    }
+);
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;

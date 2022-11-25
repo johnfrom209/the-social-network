c477d57609f49d6716414abfc419e2f4987802e0
Jonathan Moreno <johnfrom209@gmail.com>
2022-11-25 01:53:32 -0800

    Reaction model: reactionId, reactionBody, username, createdAt, subdocument to Thought

---
+++ models/Reaction.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: ObjectId,
+            default: new ObjectId
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 200
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            // use a getter to format the timestamp on query
+        }
+    }
+)

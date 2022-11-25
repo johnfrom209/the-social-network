d7b3a701b45ddec8de2ec18f97a70885a69b25a2
Jonathan Moreno <johnfrom209@gmail.com>
2022-11-25 01:28:46 -0800

    made the user model. Has username, email, thoughts ref, friends ref, and virtuals for getting friends count

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: {
+                friendCount: {
+                    get() {
+                        return friends.length
+                    }
+                }
+            }
+        }
+    }
+)
+
+const User = model('user', userSchema);
+
+module.exports = User;
